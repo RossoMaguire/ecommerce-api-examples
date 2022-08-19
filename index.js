fad5591232bc98c07a5e0471a6187df9d0db6f2b
Ross Maguire <ross.rmdesigns@gmail.com>
2022-08-19 11:44:06 +0100

    fix: 404 and woops for unhandled routes

--- index.js
+++ index.js
@@ -65,9 +65,14 @@ app.get('/api/swell', (req, res) => {
     });
 });
 
-// All other GET requests not handled before will return our client app
-app.get('*', (req, res) => {
+// Base path will return our client app
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/dist', 'index.html'));
+});
+
+// All other not handled routes return 4040
+app.get('*', (req, res) => {
+  res.status(404).send('Woops that page cannot be found');
 });
 
 // Start your server on a specified port
